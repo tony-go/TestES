4478cd715f7a31900401444973b66664e14c4fd1
Tony Gorez <gorez.tony@gmail.com>
2024-05-27 16:08:42 +0200

    Initial Commit

---
+++ TestES/TestESApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestESApp.swift
+//  TestES
+//
+//  Created by Tony Gorez on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
